b8f9d9ddee18a6e10bfe5505c36e9d122f265216
Diego lanes <diegosilvalanes@hotmail.com>
2019-08-12 22:59:40 -0300

    App 001 Concluido

---
+++ App1_ConsultaCEP/App1_ConsultaCEP/App1_ConsultaCEP/App1_ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1_ConsultaCEP.Servico.Modelo;
+using App1_ConsultaCEP.Servico;
+
+
+
+
+namespace App1_ConsultaCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+            
+        }
+
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            // TODO - Logica do programa
+
+            string cep = CEP.Text.Trim();
+
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            RESULTADO.Text = "Endereço: " + end.logradouro + ", " + end.bairro + " " + end.localidade + " - " + end.uf;
+
+        }
+    }
+}
---
+++ App1_ConsultaCEP/App1_ConsultaCEP/App1_ConsultaCEP/App1_ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App1_ConsultaCEP.Servico
+{
+    class ViaCEPServico
+    {
+        private static string enderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(enderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+    }
+}
